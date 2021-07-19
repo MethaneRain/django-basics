f50b7c9923bf88903ee9bf54bb8b9531da020cbb
MethaneRain <36716747+MethaneRain@users.noreply.github.com>
2021-07-19 16:36:14 +0100

    Create models.py

---
+++ src/products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    #title = models.TextField()
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10000,decimal_places=2)
+    summary = models.TextField(default="You all can fuck right off!")
+
+    upload = models.FileField(blank=True,upload_to='uploads/')
+    # or...
+    # file will be saved to MEDIA_ROOT/uploads/2015/01/30
+    #upload = models.FileField(upload_to='uploads/%Y/%m/%d/')
